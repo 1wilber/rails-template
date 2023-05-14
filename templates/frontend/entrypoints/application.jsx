0fcaa7986bda906b4000f75e9d316ce252cb3e6a
h4k1 <wilber.garcia@outlook.es>
2023-05-14 17:12:13 -0400

    fix: move App.jsx out of components folder

--- templates/frontend/entrypoints/application.jsx
+++ templates/frontend/entrypoints/application.jsx
@@ -2,7 +2,7 @@ console.log('Vite ⚡️ Rails')
 
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-import App from '@/components/App';
+import App from '@/App';
 
 const container = document.getElementById('root')
 const root = createRoot(container);
