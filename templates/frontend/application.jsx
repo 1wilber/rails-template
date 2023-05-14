1e34c547edd1096292e64cc336555b95c5dd52f6
h4k1 <wilber.garcia@outlook.es>
2023-05-14 16:15:51 -0400

    feat: create vite templates

---
+++ templates/frontend/application.jsx
@@ -0,0 +1,11 @@
+console.log('Vite ⚡️ Rails')
+
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+import App from '@/components/App';
+
+const container = document.getElementById('root')
+const root = createRoot(container);
+root.render(
+  <App />
+)
