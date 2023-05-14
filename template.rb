82c44f76fb794922b75d441743757f4c9af6f0f8
h4k1 <wilber.garcia@outlook.es>
2023-05-14 17:52:47 -0400

    feat: add vite + react for rails template

---
+++ template.rb
@@ -0,0 +1,70 @@
+# rails new app-name --database=postgresql -T --skip-sprockets -J 
+
+def add_vite
+  run 'bundle exec vite install'
+
+  say '[+] Downloading vite.config.js', :yellow
+  remove_file 'vite.config.js'
+  run 'wget -nv -O vite.config.js https://raw.githubusercontent.com/H4K1/rails-template/master/templates/vite.config.js'
+end
+
+def react_import
+  say '[+] Installing React', :yellow
+  run 'yarn add react react-dom'
+
+  say '[+] Importing React on the application.html.erb', :yellow
+  gsub_file "app/views/layouts/application.html.erb", /<%= vite_javascript_tag 'application' %>\n/, "<%= vite_javascript_tag 'application.jsx' %>"
+  gsub_file "app/views/layouts/application.html.erb", /<%= stylesheet_link_tag 'application', media: 'all' %>\n/, ""
+end
+
+def react_templates
+  say '[+] Copying React app', :yellow
+  from = "https://github.com/H4K1/rails-template/trunk/templates/frontend/"
+  to = "app/frontend"
+
+  run "rm -rf app/frontend"
+
+  run "svn checkout #{from} #{to} --force"
+end
+
+def generate_root
+  say '[+] Generating root page', :yellow
+  generate(:controller, 'pages', 'index')
+  run 'echo "" > app/views/pages/index.html.erb'
+  inject_into_file 'app/views/pages/index.html.erb', '<div id="root"></div>'
+  route "root to: 'pages#index'"
+end
+
+def add_react
+  react_import
+  react_templates
+  generate_root
+end
+
+
+def copy_gemfile
+  say '[+] Downloading Gemfile template', :yellow
+  remove_file 'Gemfile'
+  run 'wget -nv https://raw.githubusercontent.com/H4K1/rails-template/master/templates/Gemfile'
+end
+
+def copy_templates
+  copy_gemfile
+end
+
+def welcome_message
+  say "\n\n\n\n"
+  say 'Welcome to the Rails template!', :green
+  say 'You can getting started with the following commands:', :green
+  say 'foreman start -f Procfile.dev'
+end
+
+copy_templates
+
+after_bundle do
+  add_vite
+  add_react
+
+  welcome_message
+end
+
